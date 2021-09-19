02bc5cc40ca44df915cc8d4e6dda67a1ca733535
Jason Skollingsberg <dragonleech@gmail.com>
2021-09-19 11:06:53 -0400

    start of MundyMath

---
+++ MundyMath.ts
@@ -0,0 +1,104 @@
+enum CalcTypes {
+    ADD = 'add',
+    SUBTRACT = 'subtract',
+    MULTIPLY = 'multiply',
+    DIVIDE = 'divide',
+    POW = 'pow',
+    SQRT = 'sqrt',
+}
+
+export function add(a: number | Array<number>, b: number | Array<number>): number | Array<number> {
+    return calculate(CalcTypes.ADD, a, b)
+}
+export function subtract(a: number | Array<number>, b: number | Array<number>): number | Array<number> {
+    return calculate(CalcTypes.SUBTRACT, a, b)
+}
+
+const MundyMath = {
+    add: (a: number | Array<number>, b: number | Array<number>): number | Array<number> => {
+        return calculate(CalcTypes.ADD, a, b)
+    },
+
+    subtract: (a: number | Array<number>, b: number | Array<number>): number | Array<number> => {
+        return calculate(CalcTypes.SUBTRACT, a, b)
+    },
+
+    multiply: (a: number | Array<number>, b: number | Array<number>): number | Array<number> => {
+        return calculate(CalcTypes.MULTIPLY, a, b)
+    },
+
+    divide: (a: number | Array<number>, b: number | Array<number>): number | Array<number> => {
+        return calculate(CalcTypes.DIVIDE, a, b)
+    },
+
+    pow: (a: number | Array<number>, b: number | Array<number>): number | Array<number> => {
+        return calculate(CalcTypes.POW, a, b)
+    },
+
+    sqrt: (a: number | Array<number>): number | Array<number> => {
+        return calculate(CalcTypes.SQRT, a)
+    },
+}
+
+
+function calculate(type: string, a: number | Array<number>, b: number | Array<number> | null = null): any {
+    if (type === CalcTypes.SQRT && isNumeric(a)) {
+        return doCalculation(type, a as number);
+    }
+
+    if (isNumeric(a) && isNumeric(b)) {
+        return doCalculation(type, a as number, b as number);
+    }
+
+    if (Array.isArray(a) && Array.isArray(b)) {
+        let pairs: Array<Array<number>> = iterateOverTwoArrays(a, b);
+        return pairs.map((val: Array<number>) => calculate(type, val[0], val[1]));
+    }
+
+    throw new Error("Unable to run calculation, each value must be either numeric or an array of numeric values");
+}
+
+function isNumeric(n: any): boolean {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function iterateOverTwoArrays(a: Array<number>, b: Array<number>): Array<Array<number>> {
+    const aLen: number = a.length;
+    const bLen: number = b.length;
+    const diff: number = Math.abs(aLen - bLen);
+    const zeroArray: Array<number> = Array(diff).fill(0);
+
+    if (aLen > bLen) {
+        b = b.concat(zeroArray);
+    } else if (bLen > aLen) {
+        a = a.concat(zeroArray);
+    }
+
+    return a.map((val: number, idx: number) => {
+        return [val, b[idx]];
+    })
+}
+
+function doCalculation(type: string, a: number, b: number | null = null): number {
+    if (b === null && type !== CalcTypes.SQRT) {
+        throw new Error(`Calculations of type ${type} require 2 numbers, only 1 given`);
+    }
+
+    b = b as number;
+    switch (type) {
+        case CalcTypes.ADD:
+            return a + b;
+        case CalcTypes.SUBTRACT:
+            return a - b;
+        case CalcTypes.MULTIPLY:
+            return a * b;
+        case CalcTypes.DIVIDE:
+            return a / b;
+        case CalcTypes.POW:
+            return Math.pow(a, b)
+        case CalcTypes.SQRT:
+            return Math.sqrt(a);
+        default:
+            throw new Error('Unable to run calculation')
+    }
+}
---
+++ __tests__/MundyMath.subtract.ts
@@ -0,0 +1,44 @@
+const MundyMath = require('../MundyMath');
+
+
+describe("test subtract function with single numbers", () => {
+    it("should return 5 for add(10,5)", () => {
+        expect(MundyMath.subtract(10, 5)).toBe(5);
+    });
+    it("should return -1 for subtract(2,3)", () => {
+        expect(MundyMath.subtract(2, 3)).toBe(-1);
+    });
+    it("should return 13 for subtract (5, -8)", () => {
+        expect(MundyMath.subtract(5, -8)).toBe(13);
+    });
+    it("should return 5.3 for subtract (1.3, 4)", () => {
+        expect(MundyMath.subtract(1.3, 4)).toBe(-2.7);
+    });
+});
+
+describe("test add function with Array<numbers>", () => {
+    it("should return [2, 4, 6, 8] for add([1, 2, 3, 4], [1, 2, 3, 4])", () =>{
+         expect(MundyMath.add([1, 2, 3, 4], [1, 2, 3, 4])).toStrictEqual( [2, 4, 6, 8]);
+    });
+    it("should return [2, 4, 3, 4] for add([1, 2], [1, 2, 3, 4])", () =>{
+         expect(MundyMath.add([1, 2], [1, 2, 3, 4])).toStrictEqual( [2, 4, 3, 4]);
+    });
+    it("should return [2, 4, 3, 4] for add([1, 2, 3, 4], [1, 2])", () =>{
+         expect(MundyMath.add([1, 2,  3, 4], [1, 2])).toStrictEqual( [2, 4, 3, 4]);
+    });
+});
+
+describe("Errors expected when passing none numbers to add", () => {
+    it("should throw an error for add('a', 3)", () => {
+        expect(() => {
+            MundyMath.add('a', 3)
+        }).toThrow("Unable to run calculation, each value must be either numeric or an array of numeric values")
+    });
+    it("should throw an error for add(['a'], [3])", () => {
+        expect(() => {
+            MundyMath.add(['a'], [3])
+        }).toThrow("Unable to run calculation, each value must be either numeric or an array of numeric values")
+    });
+});
+
+
